5bbab983bed8f675267d11519c81bcfb0db5e476
roshanramangiri <roshanramangiri@gmail.com>
2023-09-13 20:15:49 +0545

    Product service implemented

---
+++ product.tfvars
@@ -0,0 +1,8 @@
+service        = "product"
+container_name = "product"
+container_port = "3001"
+imageurl       = "426857564226.dkr.ecr.us-east-2.amazonaws.com/lms-order-ms:latest"
+environment    = "dev"
+application    = "product-service"
+owner          = "adex-lms"
+region         = "us-east-2"
--- variable.tf
+++ variable.tf
@@ -2,6 +2,7 @@
 # Input variables for the main.tf file
 ################################################################################
 
+
 variable "environment" {
   description = "Working application environment eg: dev, stg, prd"
   type        = string
@@ -23,5 +24,26 @@ variable "owner" {
 variable "region" {
   description = "Region be used for all the resources"
   type        = string
-  default     = "us-east-1"
+  default     = "us-east-2"
+}
+
+variable "service" {
+  description = "Name of the Service"
+  type        = string
+}
+
+
+variable "container_name" {
+  description = "Name of the container"
+  type        = string
+}
+
+variable "container_port" {
+  description = "value of the container port"
+  type        = number
+}
+
+variable "imageurl" {
+  description = "Image Url of the Image REPO"
+  type        = string
 }
